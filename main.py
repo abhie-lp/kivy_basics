cd78de275e7da930bab88cc7978a90d5d1130db4
Abhishek Tiwari <abhie.lp@gmail.com>
2022-01-09 15:50:52 +0530

    Basic stack layout

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.button import Button
+
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super(StackLayoutExample, self).__init__(**kwargs)
+        for i in range(10):
+            self.add_widget(Button(text=f'Button {i}', size_hint=(.2, .2)))
+        size_constant = dp(100)
+        for i in range(10, 20):
+            size = size_constant + i * 2
+            self.add_widget(Button(
+                text=f'BUTTON {i}', size_hint=(None, None), size=(size, size)
+            ))
 
 
 class GridLayoutExample(GridLayout):

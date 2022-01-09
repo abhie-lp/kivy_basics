36473d202e5f927f5782ddbd1ff633c80dcf4270
Abhishek Tiwari <abhie.lp@gmail.com>
2022-01-09 12:19:06 +0530

    Show button and label in box layout

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
 
 
 class MainWidget(Widget):
